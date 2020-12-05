355649ea9a66fba6a4d02d5c874d6e97375224f9
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-12-05 17:38:50 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,17 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <p>
+          Now I can render any React component on any DOM node I want using
+          ReactDOM.render
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
