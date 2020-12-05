a618b9b5240bfbd2662ea8755c237cd9ee2a4e42
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-12-05 17:58:39 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div id="main">
-        <p>
-          Now I can render any React component on any DOM node I want using
-          ReactDOM.render
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <p>
+        Now I can render any React component on any DOM node I want using
+        ReactDOM.render
+      </p>
+    </>
+  );
+};
 
 export default App;
